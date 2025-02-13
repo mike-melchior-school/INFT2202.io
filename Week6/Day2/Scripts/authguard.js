26bad1a74d195d850b31d78711407f15560f4b03
mike <mike.melchior@dcmail.ca>
2025-02-13 18:58:38 -0500

    post week6 day2, add login functionality

---
+++ Week6/Day2/Scripts/authguard.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+(function() {
+
+    if (!sessionStorage.getItem("user")) {
+        console.warn("[AUTHGUARD] Unauthorized access detected. Redirecting to login page");
+        location.href = "login.html";
+    }
+
+
+})();
