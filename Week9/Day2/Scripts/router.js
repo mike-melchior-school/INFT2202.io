540fd31307a2e2d4c18bca20dccb9db1d5397946
mike <mike.melchior@dcmail.ca>
2025-03-10 18:04:48 -0400

    update week 9

---
+++ Week9/Day2/Scripts/router.js
@@ -0,0 +1,54 @@
+"use strict";
+
+import {loadHeader} from "./header.js";
+
+export class Router {
+    constructor(routes) {
+        this.routes = routes;
+        this.init();
+    }
+
+    init() {
+        console.log(`[INFO] Navigating to: ${location.hash.slice(1)}`)
+        this.loadRoute(location.hash.slice(1));
+    }
+
+    navigate(path) {
+        location.hash = path;
+
+
+        this.loadRoute(path);
+    }
+
+    loadRoute(path) {
+        console.log(`[INFO] Loading route: ${path}`);
+
+        const basePath = path.split("#")[0];
+
+        if (!this.routes[path]) {
+            console.error(`[WARN] Route not found ${basePath}, redirecting to 404`);
+            location.hash = "/404";
+            path = "/404";
+        }
+
+        fetch(this.routes[basePath])
+            .then(res => {
+                if (!res.ok) throw new Error(`[INFO] Failed to load ${this.routes(basePath)}`);
+                return res.text();
+            })
+            .then(html => {
+                document.querySelector("main").innerHTML = html;
+
+                loadHeader().then(() => {
+                    // Dispatch a custom event to notify that a new route have been loaded.
+                    document.dispatchEvent(new CustomEvent("routeLoaded", {detail: basePath}));
+                })
+            })
+            .catch(err => {
+                console.error(`[ERROR] Error loading page: ${err}`);
+            })
+
+
+    }
+
+}
